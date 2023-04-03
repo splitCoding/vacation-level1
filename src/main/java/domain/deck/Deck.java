381c9d960fc1c2f7b1f22e4fbb9a335a3a76d1be
splitube <voicesplit@daum.net>
2023-04-03 14:56:10 +0900

    feat: Deck 생성

---
+++ src/main/java/domain/deck/Deck.java
@@ -0,0 +1,27 @@
+package domain.deck;
+
+import domain.deck.card.Card;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Deck {
+
+    private final List<Card> cards;
+    private final ShuffleStrategy shuffleStrategy;
+
+    public Deck(final List<Card> cards, final ShuffleStrategy shuffleStrategy) {
+        this.cards = new ArrayList<>(cards);
+        this.shuffleStrategy = shuffleStrategy;
+    }
+
+    public void shuffle() {
+        shuffleStrategy.shuffle(cards);
+    }
+
+    public Card draw() {
+        if (cards.size() == 0) {
+            throw new RuntimeException("더 이상 남은 카드가 없습니다.");
+        }
+        return cards.remove(0);
+    }
+}
