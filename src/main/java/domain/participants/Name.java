ab6177992385186fbc0cdc2c587158ff08f6adcf
splitube <voicesplit@daum.net>
2023-04-03 15:20:03 +0900

    feat: Name 생성

---
+++ src/main/java/domain/participants/Name.java
@@ -0,0 +1,22 @@
+package domain.participants;
+
+public final class Name {
+
+    private static final String DEALER_NAME = "딜러";
+    private final String name;
+
+    public Name(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름은 빈칸일 수 없습니다.");
+        }
+        this.name = name;
+    }
+
+    public static Name dealer() {
+        return new Name(DEALER_NAME);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/domain/participants/NameTest.java
@@ -0,0 +1,23 @@
+package domain.participants;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NameTest {
+
+    @DisplayName("딜러의 이름은 딜러로 고정되어 있다.")
+    @Test
+    void getDealerName() {
+        final Name dealer = Name.dealer();
+        Assertions.assertThat(dealer.getName()).isEqualTo("딜러");
+    }
+
+    @DisplayName("이름을 문자열로 반환한다.")
+    @Test
+    void getName() {
+        final String nameValue = "split";
+        final Name name = new Name(nameValue);
+        Assertions.assertThat(name.getName()).isEqualTo(nameValue);
+    }
+}
