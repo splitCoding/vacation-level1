5eb458bbebd486a773013baecdd82f93d8846818
splitube <voicesplit@daum.net>
2023-04-05 13:39:44 +0900

    test: Judge 상황별 승패 판단 테스트 추가

--- src/test/java/domain/JudgeTest.java
+++ src/test/java/domain/JudgeTest.java
@@ -28,9 +28,9 @@ class JudgeTest {
         participants = new Participants(players);
     }
 
-    @DisplayName("참가자들의 승패를 판단한다. (비겼을 때)")
+    @DisplayName("참가자들의 승패를 판단한다. (블랙잭으로 비겼을 때)")
     @Test
-    void judgeAll() {
+    void judgeAllDrawWithBlackjack() {
         final Judge judge = new Judge();
 
         final Dealer dealer = participants.getDealer();
@@ -46,9 +46,47 @@ class JudgeTest {
         Assertions.assertThat(judge.getDealerDrawCount()).isEqualTo(1);
     }
 
-    @DisplayName("참가자들의 승패를 판단한다. (졋을 때)")
+    @DisplayName("참가자들의 승패를 판단한다. (버스트로 비겼을 때)")
     @Test
-    void judgeAllPlayerLose() {
+    void judgeAllDrawWithBust() {
+        final Judge judge = new Judge();
+
+        final Dealer dealer = participants.getDealer();
+        dealer.drawCard(Card.CLOVER_TWO);
+        dealer.drawCard(Card.CLOVER_KING);
+        dealer.drawCard(Card.CLOVER_QUEEN);
+
+        final Player player = participants.getCurrentPlayer();
+        player.drawCard(Card.HEART_TEN);
+        player.drawCard(Card.HEART_JACK);
+        player.drawCard(Card.HEART_THREE);
+
+        judge.judgeAll(participants);
+        Assertions.assertThat(judge.getGameResultByPlayer(player)).isEqualTo(GameResult.DRAW);
+        Assertions.assertThat(judge.getDealerDrawCount()).isEqualTo(1);
+    }
+
+    @DisplayName("참가자들의 승패를 판단한다. (스탠드로 비겼을 때)")
+    @Test
+    void judgeAllDrawWithStand() {
+        final Judge judge = new Judge();
+
+        final Dealer dealer = participants.getDealer();
+        dealer.drawCard(Card.CLOVER_FIVE);
+        dealer.drawCard(Card.CLOVER_KING);
+
+        final Player player = participants.getCurrentPlayer();
+        player.drawCard(Card.HEART_FIVE);
+        player.drawCard(Card.HEART_TEN);
+
+        judge.judgeAll(participants);
+        Assertions.assertThat(judge.getGameResultByPlayer(player)).isEqualTo(GameResult.DRAW);
+        Assertions.assertThat(judge.getDealerDrawCount()).isEqualTo(1);
+    }
+
+    @DisplayName("참가자들의 승패를 판단한다. (버스트로 졋을 때)")
+    @Test
+    void judgeAllPlayerLoseWithBust() {
         final Judge judge = new Judge();
 
         final Dealer dealer = participants.getDealer();
@@ -56,7 +94,8 @@ class JudgeTest {
         dealer.drawCard(Card.CLOVER_KING);
 
         final Player player = participants.getCurrentPlayer();
-        player.drawCard(Card.HEART_ACE);
+        player.drawCard(Card.HEART_QUEEN);
+        player.drawCard(Card.HEART_FIVE);
         player.drawCard(Card.HEART_NINE);
 
         judge.judgeAll(participants);
@@ -64,9 +103,27 @@ class JudgeTest {
         Assertions.assertThat(judge.getDealerWinCount()).isEqualTo(1);
     }
 
-    @DisplayName("참가자들의 승패를 판단한다. (이겼을 때)")
+    @DisplayName("참가자들의 승패를 판단한다. (스탠드로 졋을 때)")
     @Test
-    void judgeAllPlayerWin() {
+    void judgeAllPlayerLoseWithStand() {
+        final Judge judge = new Judge();
+
+        final Dealer dealer = participants.getDealer();
+        dealer.drawCard(Card.CLOVER_SEVEN);
+        dealer.drawCard(Card.CLOVER_KING);
+
+        final Player player = participants.getCurrentPlayer();
+        player.drawCard(Card.HEART_QUEEN);
+        player.drawCard(Card.HEART_FIVE);
+
+        judge.judgeAll(participants);
+        Assertions.assertThat(judge.getGameResultByPlayer(player)).isEqualTo(GameResult.LOSE);
+        Assertions.assertThat(judge.getDealerWinCount()).isEqualTo(1);
+    }
+
+    @DisplayName("참가자들의 승패를 판단한다. (블랙잭으로 이겼을 때)")
+    @Test
+    void judgeAllPlayerWinWithBlackjack() {
         final Judge judge = new Judge();
 
         final Dealer dealer = participants.getDealer();
@@ -76,6 +133,24 @@ class JudgeTest {
         final Player player = participants.getCurrentPlayer();
         player.drawCard(Card.HEART_ACE);
         player.drawCard(Card.HEART_TEN);
+
+        judge.judgeAll(participants);
+        Assertions.assertThat(judge.getGameResultByPlayer(player)).isEqualTo(GameResult.WIN);
+        Assertions.assertThat(judge.getDealerLoseCount()).isEqualTo(1);
+    }
+
+    @DisplayName("참가자들의 승패를 판단한다. (스탠드로 이겼을 때)")
+    @Test
+    void judgeAllPlayerWinWithStand() {
+        final Judge judge = new Judge();
+
+        final Dealer dealer = participants.getDealer();
+        dealer.drawCard(Card.CLOVER_ACE);
+        dealer.drawCard(Card.CLOVER_SEVEN);
+
+        final Player player = participants.getCurrentPlayer();
+        player.drawCard(Card.HEART_ACE);
+        player.drawCard(Card.HEART_NINE);
 
         judge.judgeAll(participants);
         Assertions.assertThat(judge.getGameResultByPlayer(player)).isEqualTo(GameResult.WIN);
